58e7d833004a589123db01b1620f22ff19661b15
guillebonutto <guillebonutto@hotmail.com>
2022-04-01 19:49:11 -0300

    agregando errores

--- src/main/resources/static/lib/index.js
+++ src/main/resources/static/lib/index.js
@@ -43,23 +43,7 @@ var app = new Vue({
 					} else window.location.href = "web/accounts.html"
 				})
 				.catch((error) => {
-					// if (error.response) {
-					// 	// The request was made and the server responded with a status code
-					// 	// that falls out of the range of 2xx
-					// 	console.log(error.response.data)
-					// 	console.log(error.response.status)
-					// 	console.log(error.response.headers)
-					// } else if (error.request) {
-					// 	// The request was made but no response was received
-					// 	// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-					// 	// http.ClientRequest in node.js
-					// 	console.log(error.request)
-					// } else {
-					// 	// Something happened in setting up the request that triggered an Error
-					// 	console.log("Error", error.message)
-					// }
-					// handle error
-					if (!this.email && !this.password) {
+					if (error.response == "Missing data") {
 						Swal.fire({
 							icon: "error",
 							title: "Oops...",
@@ -114,10 +98,25 @@ var app = new Vue({
 						text: "Your registration was successfull",
 						timer: 1500,
 					}).then(() => {
-					    this.email = this.emailRegister
-					    this.password = this.passwordRegister
+						this.email = this.emailRegister
+						this.password = this.passwordRegister
 						this.login()
 					})
+				})
+				.catch((error) => {
+					if (error.response == "Missing data") {
+						Swal.fire({
+							icon: "error",
+							title: "Oops...",
+							text: "No data received. Please check the fields and try again",
+						})
+					} else {
+						Swal.fire({
+							icon: "error",
+							text: error.response.data,
+							timer: 1500,
+						})
+					}
 				})
 		},
 		ocultar() {
