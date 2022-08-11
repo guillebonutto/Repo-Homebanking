0ca6a2014c646d6e998908e948dfc1021076b7d2
guillebonutto <guillebonutto@hotmail.com>
2022-08-10 23:12:13 -0300

    reparando bugs(1)

--- src/main/resources/static/lib/accounts.js
+++ src/main/resources/static/lib/accounts.js
@@ -1,23 +1,23 @@
-let text = document.getElementsByClassName("texto")
-let cards = document.getElementsByClassName("card")
-let divprincipal = document.getElementById("div-principal")
-let sombras = document.getElementsByClassName("shadow")
+let text = document.getElementsByClassName('texto')
+let cards = document.getElementsByClassName('card')
+let divprincipal = document.getElementById('div-principal')
+let sombras = document.getElementsByClassName('shadow')
 
 var app = new Vue({
-	el: "#app",
+	el: '#app',
 	data: {
-		message: "Hello Vue!",
+		message: 'Hello Vue!',
 		nightmode: 0,
 		clients: [],
 		accounts: [],
-		type: "Select",
+		type: 'Select',
 		showModal: true,
 		loadingPage: true,
 		loans: [],
 		totalBalance: 0,
-		filterStatus: "",
+		filterStatus: '',
 		mobile: false,
-		collap: "menu-collapsed",
+		collap: 'menu-collapsed',
 	},
 	created() {
 		this.loadData()
@@ -27,42 +27,42 @@ var app = new Vue({
 	methods: {
 		chequeo() {
 			for (let h = 0; h < cards.length; h++) {
-				cards[h].style.color = "#4f4f4f"
+				cards[h].style.color = '#4f4f4f'
 			}
 			if (this.nightmode == 1) {
 				for (let g = 0; g < sombras.length; g++) {
-					sombras[g].style.boxShadow = "0.5rem 1rem white !important"
+					sombras[g].style.boxShadow = '0.5rem 1rem white !important'
 				}
-				body1.style.backgroundColor = "#4f4f4f"
+				body1.style.backgroundColor = '#4f4f4f'
 				for (let i = 0; i < text.length; i++) {
-					text[i].style.color = "white"
+					text[i].style.color = 'white'
 				}
-				noche.style.color = "white"
-				noche.style.fontWeight = "bold"
+				noche.style.color = 'white'
+				noche.style.fontWeight = 'bold'
 				// titulo.style.textShadow = "2px 2px 8px white"
 			} else {
 				for (let g = 0; g < sombras.length; g++) {
-					sombras[g].style.boxShadow = "0.5rem 1rem rgb(0,0,0,.15) !important"
+					sombras[g].style.boxShadow = '0.5rem 1rem rgb(0,0,0,.15) !important'
 				}
-				body1.style.backgroundColor = "white"
+				body1.style.backgroundColor = 'white'
 				for (let i = 0; i < text.length; i++) {
-					text[i].style.color = "black"
+					text[i].style.color = 'black'
 				}
-				noche.style.color = "black"
-				noche.style.fontWeight = ""
+				noche.style.color = 'black'
+				noche.style.fontWeight = ''
 				// titulo.style.textShadow = "2px 2px 7px black"
 			}
 		},
 		check() {
-			if (this.collap == "menu-collapsed") {
-				this.collap = "menu-expanded"
+			if (this.collap == 'menu-collapsed') {
+				this.collap = 'menu-expanded'
 			} else {
-				this.collap = "menu-collapsed"
+				this.collap = 'menu-collapsed'
 			}
 		},
 		loadData() {
 			axios
-				.get("/api/clients/current")
+				.get('/api/clients/current')
 				.then((response) => {
 					// handle success
 					this.showModal = false
@@ -71,7 +71,7 @@ var app = new Vue({
 					this.firstName = response.data.firstName
 					this.lastName = response.data.lastName
 					this.email = response.data.email
-					photo.setAttribute("src", this.clients.photo)
+					photo.setAttribute('src', this.clients.photo)
 					this.accounts = response.data.accounts
 					this.loans = response.data.loans
 					this.filter()
@@ -85,35 +85,35 @@ var app = new Vue({
 				})
 		},
 		signOut() {
-			axios.post("/api/logout").then(() => {
-				window.location.href = "/index.html"
+			axios.post('/api/logout').then(() => {
+				window.location.href = '/index.html'
 			})
 		},
 		redirectIfExist(pagina) {
 			axios
-				.get("/web/" + pagina + ".html")
+				.get('/web/' + pagina + '.html')
 				.then(() => {
-					window.location.href = "/web/" + pagina + ".html"
+					window.location.href = '/web/' + pagina + '.html'
 				})
 				.catch((error) => {
 					if (error.response.status == 404) {
-						window.location.href = "/web/NotFound.html"
+						window.location.href = '/web/NotFound.html'
 					}
 				})
 		},
 		link(link) {
 			var miUrl = `/web/account.html?id=${link.id}`
-			window.open(miUrl, "_self")
+			window.open(miUrl, '_self')
 		},
 		addAccount() {
 			// this.type = this.type.toUpperCase()
 			// this.type = this.type.replace(" ", "_")
 			axios
-				.post("/api/clients/current/accounts", `type=${this.type}`)
+				.post('/api/clients/current/accounts', `type=${this.type}`)
 				.then(() => {
 					Swal.fire({
-						icon: "success",
-						text: "You created a new account",
+						icon: 'success',
+						text: 'You created a new account',
 					}).then(() => {
 						this.showModal = false
 						window.location.reload()
@@ -121,18 +121,18 @@ var app = new Vue({
 				})
 				.catch((err) => {
 					Swal.fire({
-						icon: "error",
-						title: "Error",
-						text: "An error occurred in the proccess 🥺 try again and if the error persist don not hesitate to contact us 😊",
+						icon: 'error',
+						title: 'Error',
+						text: 'An error occurred in the proccess 🥺 try again and if the error persist don not hesitate to contact us 😊',
 					})
 				})
 		},
 		hiddeAccount(id) {
-			axios.patch(`/api/clients/current/accounts/${id}`, "statusHide=true").then(() => {
+			axios.patch(`/api/clients/current/accounts/${id}`, 'statusHide=true').then(() => {
 				Swal.fire({
-					icon: "success",
-					title: "Success",
-					text: "Your account was deleted",
+					icon: 'success',
+					title: 'Success',
+					text: 'Your account was deleted',
 				}).then(() => {
 					window.location.reload()
 				})
@@ -151,32 +151,32 @@ var app = new Vue({
 })
 
 if (screen.width > 800) {
-	app.collap = "menu-expanded"
+	app.collap = 'menu-expanded'
 	app.mobile = false
 } else {
-	app.collap = "menu-collapsed"
+	app.collap = 'menu-collapsed'
 	app.mobile = true
 }
 
 window.onresize = function () {
 	if (screen.width > 800) {
-		app.collap = "menu-expanded"
+		app.collap = 'menu-expanded'
 		app.mobile = false
 	} else {
-		app.collap = "menu-collapsed"
+		app.collap = 'menu-collapsed'
 		app.mobile = true
 	}
 }
 
 console.log(
-	"%cWARNING!",
-	"color: yellow;text-shadow: -1px 0 black, 0 2px black, 2px 0 black, 0 -1px black;font-size: 45px;font-weight:bold"
+	'%cWARNING!',
+	'color: yellow;text-shadow: -1px 0 black, 0 2px black, 2px 0 black, 0 -1px black;font-size: 45px;font-weight:bold'
 )
 console.log(
-	"%cIf someone told you to copy/paste something here you are going to be scammed",
-	"font-family: Arial, sans-serif;font-size: 18px;font-weight:bold"
+	'%cIf someone told you to copy/paste something here you are going to be scammed',
+	'font-family: Arial, sans-serif;font-size: 18px;font-weight:bold'
 )
 console.log(
-	"%cPasting anything in here could give attackers access to your Ecobank account",
-	"color:red;font-size: 19px;font-family: Arial, sans-serif;font-weight:bold"
+	'%cPasting anything in here could give attackers access to your Ecobank account',
+	'color:red;font-size: 19px;font-family: Arial, sans-serif;font-weight:bold'
 )
--- src/main/resources/static/lib/notFound.js
+++ src/main/resources/static/lib/notFound.js
@@ -1,21 +1,21 @@
-let text = document.getElementsByClassName("texto")
-let cards = document.getElementsByClassName("card")
-let divtable = document.getElementsByClassName("div-tabla")
-let divprincipal = document.getElementById("div-principal")
+let text = document.getElementsByClassName('texto')
+let cards = document.getElementsByClassName('card')
+let divtable = document.getElementsByClassName('div-tabla')
+let divprincipal = document.getElementById('div-principal')
 
 var app = new Vue({
-	el: "#app",
+	el: '#app',
 	data: {
-		message: "Hello Vue!",
+		message: 'Hello Vue!',
 		nightmode: 0,
 		showModal: true,
 		loadingPage: true,
-		fecha: "",
+		fecha: '',
 		clients: [],
-		firstName: "",
-		lastName: "",
-		email: "",
-		collap: "menu-expanded",
+		firstName: '',
+		lastName: '',
+		email: '',
+		collap: 'menu-expanded',
 	},
 	created() {
 		this.loadData()
@@ -24,32 +24,32 @@ var app = new Vue({
 	methods: {
 		chequeo() {
 			if (this.nightmode == 1) {
-				body1.style.backgroundColor = "#4f4f4f"
+				body1.style.backgroundColor = '#4f4f4f'
 				for (let i = 0; i < text.length; i++) {
-					text[i].style.color = "white"
+					text[i].style.color = 'white'
 				}
-				noche.style.color = "white"
-				noche.style.fontWeight = "bold"
-				titulo.style.textShadow = "2px 2px 8px white"
+				noche.style.color = 'white'
+				noche.style.fontWeight = 'bold'
+				titulo.style.textShadow = '2px 2px 8px white'
 			} else {
-				body1.style.backgroundColor = "white"
+				body1.style.backgroundColor = 'white'
 				for (let i = 0; i < text.length; i++) {
-					text[i].style.color = "black"
+					text[i].style.color = 'black'
 				}
-				noche.style.color = "black"
-				noche.style.fontWeight = ""
-				titulo.style.textShadow = "2px 2px 7px black"
+				noche.style.color = 'black'
+				noche.style.fontWeight = ''
+				titulo.style.textShadow = '2px 2px 7px black'
 			}
 		},
 		check() {
-			if (this.collap == "menu-collapsed") {
-				this.collap = "menu-expanded"
+			if (this.collap == 'menu-collapsed') {
+				this.collap = 'menu-expanded'
 			} else {
-				this.collap = "menu-collapsed"
+				this.collap = 'menu-collapsed'
 			}
 		},
 		loadData() {
-			axios.get("/api/clients/current").then((response) => {
+			axios.get('/api/clients/current').then((response) => {
 				// handle success
 				this.showModal = false
 				this.loadingPage = false
@@ -57,24 +57,24 @@ var app = new Vue({
 				this.firstName = response.data.firstName
 				this.lastName = response.data.lastName
 				this.email = response.data.email
-				let photo = document.querySelector("#photo")
-				photo.setAttribute("src", this.clients.photo)
+				let photo = document.querySelector('#photo')
+				photo.setAttribute('src', this.clients.photo)
 			})
 		},
 		signOut() {
-			axios.post("/api/logout").then(() => {
-				window.location.href = "/index.html"
+			axios.post('/api/logout').then(() => {
+				window.location.href = '/index.html'
 			})
 		},
 		redirectIfExist(pagina) {
 			axios
-				.get("/web/" + pagina + ".html")
+				.get('/web/' + pagina + '.html')
 				.then(() => {
-					window.location.href = "/web/" + pagina + ".html"
+					window.location.href = '/web/' + pagina + '.html'
 				})
 				.catch((error) => {
 					if (error.response.status == 404) {
-						window.location.href = "/web/NotFound.html"
+						window.location.href = '/web/NotFound.html'
 					}
 				})
 		},
@@ -83,28 +83,28 @@ var app = new Vue({
 
 window.onresize = function () {
 	if (screen.width > 800) {
-		app.collap = "menu-expanded"
+		app.collap = 'menu-expanded'
 	} else {
-		app.collap = ""
-		app.collap = "menu-collapsed"
+		app.collap = ''
+		app.collap = 'menu-collapsed'
 	}
 }
 
 if (screen.width > 800) {
-	app.collap = "menu-expanded"
+	app.collap = 'menu-expanded'
 } else {
-	app.collap = "menu-collapsed"
+	app.collap = 'menu-collapsed'
 }
 
 console.log(
-	"%cWARNING!",
-	"color: yellow;text-shadow: -1px 0 black, 0 2px black, 2px 0 black, 0 -1px black;font-size: 45px;font-weight:bold"
+	'%cWARNING!',
+	'color: yellow;text-shadow: -1px 0 black, 0 2px black, 2px 0 black, 0 -1px black;font-size: 45px;font-weight:bold'
 )
 console.log(
-	"%cIf someone told you to copy/paste something here you are going to be scammed",
-	"font-size: 15px;font-weight:bold"
+	'%cIf someone told you to copy/paste something here you are going to be scammed',
+	'font-size: 15px;font-weight:bold'
 )
 console.log(
-	"%cPasting anything in here could give attackers access to your Ecobank account",
-	"color:red;font-size: 16px;font-weight:bold"
+	'%cPasting anything in here could give attackers access to your Ecobank account',
+	'color:red;font-size: 16px;font-weight:bold'
 )
